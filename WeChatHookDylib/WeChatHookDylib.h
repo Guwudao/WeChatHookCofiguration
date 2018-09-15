db295326ad5ccc7accb85016c9d0bf1bd3d98d97
Jackie <13265466114@163.com>
2018-09-15 22:17:06 +0800

    Initial Commit

---
+++ WeChatHookDylib/WeChatHookDylib.h
@@ -0,0 +1,13 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  WeChatHookDylib.h
+//  WeChatHookDylib
+//
+//  Created by Jackie on 2018/9/15.
+//  Copyright (c) 2018年 Jackie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
---
+++ WeChatHookDylib/WeChatHookDylib.m
@@ -0,0 +1,56 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  WeChatHookDylib.m
+//  WeChatHookDylib
+//
+//  Created by Jackie on 2018/9/15.
+//  Copyright (c) 2018年 Jackie. All rights reserved.
+//
+
+#import "WeChatHookDylib.h"
+#import <CaptainHook/CaptainHook.h>
+#import <UIKit/UIKit.h>
+#import <Cycript/Cycript.h>
+
+static __attribute__((constructor)) void entry(){
+    NSLog(@"\n               🎉!!！congratulations!!！🎉\n👍----------------insert dylib success----------------👍");
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidFinishLaunchingNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification * _Nonnull note) {
+        
+#ifndef __OPTIMIZE__
+        CYListenServer(6666);
+#endif
+        
+    }];
+}
+
+@interface CustomViewController
+
+-(NSString*)getMyName;
+
+@end
+
+CHDeclareClass(CustomViewController)
+
+CHOptimizedMethod(0, self, NSString*, CustomViewController,getMyName){
+    //get origin value
+    NSString* originName = CHSuper(0, CustomViewController, getMyName);
+    
+    NSLog(@"origin name is:%@",originName);
+    
+    //get property
+    NSString* password = CHIvar(self,_password,__strong NSString*);
+    
+    NSLog(@"password is %@",password);
+    
+    //change the value
+    return @"AloneMonkey";
+    
+}
+
+CHConstructor{
+    CHLoadLateClass(CustomViewController);
+    CHClassHook(0, CustomViewController, getMyName);
+}
+
